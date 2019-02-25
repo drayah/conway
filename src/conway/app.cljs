1c3a99a2d6da35a33729d273a2ce2be40cf6334c
Giovanni Martina <drayah@gmail.com>
2019-02-24 21:34:10 -0300

    Initialize world and set state

--- src/conway/app.cljs
+++ src/conway/app.cljs
@@ -1,4 +1,17 @@
-(ns conway.app)
+(ns conway.app
+  (:require [conway.protocols.world :as p-world]
+            [conway.components.game-world :as game]))
+
+(def size 3)
+
+(def seed [0 0 0
+           0 0 0
+           0 0 0])
+
+(def game-world (game/create-game-world))
 
 (defn init []
-  (js/console.log "hi conway"))
+  (let [initial-world (p-world/initialize! game-world size seed)]
+    (js/console.log "Initialized world of size " (:size initial-world))
+    (doseq [cell (:iteration initial-world)]
+      (js/console.log cell))))
--- src/conway/components/game_world.cljs
+++ src/conway/components/game_world.cljs
@@ -2,13 +2,20 @@
   (:require [conway.protocols.world :as p-world]
             [conway.validations.world :refer [valid? explain seed?]]))
 
+(defn- initialize-game-world!
+  [{:keys [size seed] :as params}
+   state]
+  (and (valid? seed? params)
+       (swap! state (constantly {:size      size
+                                 :iteration seed}))))
+
 (defrecord GameWorld [state]
   p-world/IWorld
 
   (initialize! [_ size seed]
     (let [params {:size size
                   :seed seed}]
-      (or (valid? seed? params)
+      (or (initialize-game-world! params state)
           (throw (ex-info
                   "World initialization failed!"
                   {:error (explain seed? params)})))))
@@ -18,4 +25,4 @@
   (current [_]))
 
 (defn create-game-world []
-  (->GameWorld (atom [])))
+  (->GameWorld (atom {})))
