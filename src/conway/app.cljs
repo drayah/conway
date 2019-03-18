27d0069c314a2a87f2f7949be9a687b521633baa
Giovanni Martina <drayah@gmail.com>
2019-03-18 20:51:02 -0300

    Get 2d canvas context as surface

--- src/conway/app.cljs
+++ src/conway/app.cljs
@@ -5,10 +5,14 @@
             [conway.dom.document :as d]
             [conway.game.loop :as g-loop]))
 
+(defn- surface! [canvas-id context-type]
+  (-> (d/element-by-id! canvas-id)
+      (.getContext context-type)))
+
 (defn- run-game! []
   (let [{:keys [size seed]} seeds/simple-configuration
         world               (game/empty-world)
-        surface             (d/element-by-id! "game-canvas")
+        surface             (surface! "game-canvas" "2d")
         _                   (p-world/initialize! world size seed)]
     (js/console.log "Initialized world of size " (p-world/size world))
     (g-loop/forever! world surface)))
