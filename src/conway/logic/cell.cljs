89d1a131bd437f5d971caf920cf9694b503eb7e7
Giovanni Martina <drayah@gmail.com>
2019-05-06 21:53:43 -0300

    Add stub cell logic

---
+++ src/conway/logic/cell.cljs
@@ -0,0 +1,13 @@
+(ns conway.logic.cell)
+
+(defn under-population?
+  [neighbors])
+
+(defn over-population?
+  [neighbors])
+
+(defn stay-alive?
+  [cell neighbors])
+
+(defn revive?
+  [cell neighbors])
