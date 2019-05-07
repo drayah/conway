8c0b31366b8e5b568f8cb87cdc2da3257f3a175a
Giovanni Martina <drayah@gmail.com>
2019-05-06 21:54:18 -0300

    Change require alias

--- test/conway/logic/world_test.cljs
+++ test/conway/logic/world_test.cljs
@@ -1,12 +1,12 @@
 (ns conway.logic.world-test
-  (:require [conway.logic.world :as l-world]
+  (:require [conway.logic.world :as logic.world]
             [cljs.test :refer-macros [deftest testing is are]]
             [matcher-combinators.test]))
 
 (def neighbor-count 8)
 
 (deftest valid-seed?
-  (are [params result] (= (l-world/valid-seed? params) result)
+  (are [params result] (= (logic.world/valid-seed? params) result)
     {:size 0 :seed []}                  true
     {:size 2 :seed [0 0 0 0]}           true
     {:size 2 :seed [1 0 0 0]}           true
@@ -32,7 +32,7 @@
   (map ->coordinate params))
 
 (deftest generation->cell+coordinates
-  (are [generation size result] (= (l-world/generation->cell+coordinates size generation) result)
+  (are [generation size result] (= (logic.world/generation->cell+coordinates size generation) result)
     [0 0 0 0]           2 (coords [0 0 0] [0 1 0] [0 0 1] [0 1 1])
     [0 1 0 1]           2 (coords [0 0 0] [1 1 0] [0 0 1] [1 1 1])
     [1 0 1 0]           2 (coords [1 0 0] [0 1 0] [1 0 1] [0 1 1])
@@ -40,7 +40,7 @@
     [0 0 0 1 1 1 0 0 0] 3 (coords [0 0 0] [0 1 0] [0 2 0] [1 0 1] [1 1 1] [1 2 1] [0 0 2] [0 1 2] [0 2 2])))
 
 (deftest coordinates->generation-index
-  (are [coordinates size result] (= (l-world/coordinates->generation-index coordinates size) result)
+  (are [coordinates size result] (= (logic.world/coordinates->generation-index coordinates size) result)
     {:x 0 :y 0} 2 0
     {:x 1 :y 0} 2 1
     {:x 0 :y 1} 2 2
@@ -48,7 +48,7 @@
     {:x 1 :y 2} 5 11))
 
 (deftest coordinates->vertical-neighbors
-  (are [coordinates size result] (= (l-world/coordinates->vertical-neighbors coordinates size) result)
+  (are [coordinates size result] (= (logic.world/coordinates->vertical-neighbors coordinates size) result)
     {:x 0 :y 0} 3 [{:x 0 :y 2} {:x 0 :y 1}]
     {:x 1 :y 0} 3 [{:x 1 :y 2} {:x 1 :y 1}]
     {:x 2 :y 0} 3 [{:x 2 :y 2} {:x 2 :y 1}]
@@ -60,7 +60,7 @@
     {:x 2 :y 2} 3 [{:x 2 :y 1} {:x 2 :y 0}]))
 
 (deftest coordinates->horizontal-neighbors
-  (are [coordinates size result] (= (l-world/coordinates->horizontal-neighbors coordinates size) result)
+  (are [coordinates size result] (= (logic.world/coordinates->horizontal-neighbors coordinates size) result)
     {:x 0 :y 0} 3 [{:x 2 :y 0} {:x 1 :y 0}]
     {:x 1 :y 0} 3 [{:x 0 :y 0} {:x 2 :y 0}]
     {:x 2 :y 0} 3 [{:x 1 :y 0} {:x 0 :y 0}]
@@ -72,18 +72,18 @@
     {:x 2 :y 2} 3 [{:x 1 :y 2} {:x 0 :y 2}]))
 
 (deftest coordinates->diagonal-neighbors
-  (are [coordinates size result] (= (l-world/coordinates->diagonal-neighbors coordinates size) result)
+  (are [coordinates size result] (= (logic.world/coordinates->diagonal-neighbors coordinates size) result)
     {:x 0 :y 0} 3 [{:x 2 :y 2} {:x 2 :y 1} {:x 1 :y 2} {:x 1 :y 1}]
     {:x 1 :y 1} 3 [{:x 0 :y 0} {:x 0 :y 2} {:x 2 :y 0} {:x 2 :y 2}]))
 
 (deftest coordinates->neighbors
   (testing "returns correct neighbor count"
-    (is (= (-> (l-world/coordinates->neighbors {:x 0 :y 0} 3)
+    (is (= (-> (logic.world/coordinates->neighbors {:x 0 :y 0} 3)
                count)
            neighbor-count)))
 
   (testing "returns all coordinate neighbors"
-    (are [coordinates size result] (= (l-world/coordinates->neighbors coordinates size) result)
+    (are [coordinates size result] (= (logic.world/coordinates->neighbors coordinates size) result)
       {:x 1 :y 1} 3 [{:x 1 :y 0}
                      {:x 1 :y 2}
                      {:x 0 :y 1}
@@ -122,14 +122,14 @@
   (testing "returns correct number of elements"
     (let [size       (:size mock-generation)
           generation (:generation mock-generation)]
-      (is (= (-> (l-world/generation->neighbors size generation)
+      (is (= (-> (logic.world/generation->neighbors size generation)
                  count)
              (count generation)))))
 
   (testing "returns correct neighbors and cell values"
-    (is (match? (->> (l-world/generation->neighbors 3 [:a :b :c
-                                                       :d :e :f
-                                                       :g :h :i])
+    (is (match? (->> (logic.world/generation->neighbors 3 [:a :b :c
+                                                           :d :e :f
+                                                           :g :h :i])
                      (cell-by-coords {:x 1 :y 1}))
                 {:cell      :e :x 1 :y 1
                  :neighbors [{:cell :b :x 1 :y 0}
@@ -141,9 +141,9 @@
                              {:cell :c :x 2 :y 0}
                              {:cell :i :x 2 :y 2}]}))
 
-    (is (match? (->> (l-world/generation->neighbors 3 [:a :b :c
-                                                       :d :e :f
-                                                       :g :h :i])
+    (is (match? (->> (logic.world/generation->neighbors 3 [:a :b :c
+                                                           :d :e :f
+                                                           :g :h :i])
                      (cell-by-coords {:x 0 :y 0}))
                 {:cell      :a :x 0 :y 0
                  :neighbors [{:cell :g :x 0 :y 2}
