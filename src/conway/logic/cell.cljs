a2fce1576d46cf668f762c3eba2c064a663fe4b6
Giovanni Martina <drayah@gmail.com>
2019-05-07 21:49:26 -0300

    Refactor dryness

--- src/conway/logic/cell.cljs
+++ src/conway/logic/cell.cljs
@@ -3,28 +3,37 @@
 (def state {:dead  0
             :alive 1})
 
+(def neighbor-count {:minimum 2
+                     :maximum 3})
+
 (defn- alive? [cell]
   (= cell (:alive state)))
 
 (def filter-alive (partial filter (comp alive? :cell)))
 
-(defn under-population?
+(defn- count-alive
   [neighbors]
   (->> neighbors
        filter-alive
-       count
-       (> 2)))
+       count))
+
+(defn under-population?
+  [neighbors]
+  (-> neighbors
+      count-alive
+      (< (:minimum neighbor-count))))
 
 (defn over-population?
   [neighbors]
-  (->> neighbors
-       filter-alive
-       count
-       (< 3)))
+  (-> neighbors
+      count-alive
+      (> (:maximum neighbor-count))))
 
 (defn stay-alive?
   [cell neighbors])
 
 (defn revive?
   [neighbors]
-  (->> neighbors))
+  (-> neighbors
+      count-alive
+      (= (:maximum neighbor-count))))
