a4f8ca3a774c2a255bd6a615baeba96f1980d484
Giovanni Martina <drayah@gmail.com>
2019-05-07 21:55:44 -0300

    Add revive? test

--- test/conway/logic/cell_test.cljs
+++ test/conway/logic/cell_test.cljs
@@ -26,3 +26,15 @@
     (coords [1 0 0] [1 0 1] [0 1 0] [0 1 1]) false
     (coords [1 0 0] [1 0 1] [0 1 0] [1 1 1]) false
     (coords [1 0 0] [1 0 1] [1 1 0] [1 1 1]) true))
+
+(deftest revive?
+  (are [neighbors result] (= (logic.cell/revive? neighbors) result)
+    (coords)                                 false
+    (coords [0 0 0])                         false
+    (coords [1 0 0])                         false
+    (coords [0 0 0] [0 1 0] [0 1 1])         false
+    (coords [0 0 0] [1 1 0] [1 1 1])         false
+    (coords [1 0 0] [1 1 0] [1 1 1])         true
+    (coords [1 0 0] [1 1 0] [0 0 1] [1 1 1]) true
+    (coords [1 0 0] [0 1 0] [1 0 1] [1 1 1]) true
+    (coords [1 0 0] [1 1 0] [1 0 1] [1 1 1]) false))
