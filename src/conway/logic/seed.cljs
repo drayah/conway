d80b4c7819dd1b20d0f719c84acb447517a2e8b6
Giovanni Martina <drayah@gmail.com>
2019-05-19 18:27:25 -0300

    Add top and bottom sides logic

--- src/conway/logic/seed.cljs
+++ src/conway/logic/seed.cljs
@@ -1,11 +1,29 @@
 (ns conway.logic.seed)
 
-(defn- calculate-fill-sides
+(def dead-cell 0)
+
+(defn- calculate-number-of-extra-sides
   [original-size final-size]
   (-> (- final-size original-size)
       (/ 2)))
 
+(defn- top
+  [num-sides size]
+  (vertical Math/floor num-sides size))
+
+(defn- bottom
+  [num-sides size]
+  (vertical Math/ceil num-sides size))
+
+(defn- vertical
+  [correct-sides-fn num-sides size]
+  (->> (repeat size dead-cell)
+       (repeat (correct-sides-fn num-sides))))
+
 (defn resize
   [configuration final-size]
-  (let [fill (calculate-fill-sides (:size configuration) final-size)]
-    fill))
+  (let [num-sides (calculate-fill-sides (:size configuration) final-size)]
+    (concat
+     (top num-sides final-size)
+     ;middle
+     (bottom num-sides final-size))))
