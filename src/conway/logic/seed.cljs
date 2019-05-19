f6534da979455a6037a07382a02dc034ecfdc678
Giovanni Martina <drayah@gmail.com>
2019-05-19 19:02:55 -0300

    Finish seed logic

--- src/conway/logic/seed.cljs
+++ src/conway/logic/seed.cljs
@@ -2,28 +2,50 @@
 
 (def dead-cell 0)
 
-(defn- calculate-number-of-extra-sides
-  [original-size final-size]
-  (-> (- final-size original-size)
+(defn- calculate-extra-sides
+  [original-size new-size]
+  (-> (- new-size original-size)
       (/ 2)))
 
-(defn- top
-  [num-sides size]
-  (vertical Math/floor num-sides size))
-
-(defn- bottom
-  [num-sides size]
-  (vertical Math/ceil num-sides size))
-
 (defn- vertical
-  [correct-sides-fn num-sides size]
-  (->> (repeat size dead-cell)
+  [correct-sides-fn num-sides new-size]
+  (->> (repeat new-size dead-cell)
        (repeat (correct-sides-fn num-sides))))
 
+(defn- top
+  [num-sides new-size]
+  (vertical Math/floor num-sides new-size))
+
+(defn- bottom
+  [num-sides new-size]
+  (vertical Math/ceil num-sides new-size))
+
+(defn- left
+  [num-sides]
+  (repeat (Math/floor num-sides) dead-cell))
+
+(defn- right
+  [num-sides]
+  (repeat (Math/ceil num-sides) dead-cell))
+
+(defn- resize-row
+  [num-sides row]
+  (concat (left num-sides) row (right num-sides)))
+
+(defn- middle
+  [num-sides rows]
+  (map (partial resize-row num-sides) rows))
+
+(defn- merge-rows
+  [resized-rows]
+  (apply concat resized-rows))
+
 (defn resize
-  [configuration final-size]
-  (let [num-sides (calculate-fill-sides (:size configuration) final-size)]
-    (concat
-     (top num-sides final-size)
-     ;middle
-     (bottom num-sides final-size))))
+  [configuration new-size]
+  (let [num-sides (calculate-extra-sides (:size configuration) new-size)
+        rows      (partition (:size configuration) (:seed configuration))]
+    {:size new-size
+     :seed (-> (concat (top num-sides new-size)
+                       (middle num-sides rows)
+                       (bottom num-sides new-size))
+               merge-rows)}))
